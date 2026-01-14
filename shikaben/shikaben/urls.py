00ddc86890d3cbfca057fd89533bc259e32995e4
Rikuto Takemura <rikuto08240824@icloud.com>
2026-01-14 16:05:58 +0900

    Enhance index view with dynamic data and update URL routing
    - Updated the index view to include years, categories by field, and update histories for better data representation.
    - Removed STATICFILES_DIRS from settings as it is no longer needed.
    - Added URL patterns for the index view and a new manual page using TemplateView.

--- shikaben/shikaben/urls.py
+++ shikaben/shikaben/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import TemplateView
+from core import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', views.index, name='index'),
+     path('doujouManual.html', TemplateView.as_view(template_name='core/doujouManual.html'), name='manual'),
 ]
